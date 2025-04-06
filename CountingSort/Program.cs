7973389d1e4cad6f66447f556770a8e6d37f8c6c
VeerShahCodes <shahveer1213@gmail.com>
2025-04-06 11:55:40 -0700

    Add project files.

---
+++ CountingSort/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace CountingSort
+{
+    internal class Program
+    {
+
+        public static List<uint> CountingSort(List<uint> data) 
+        {
+            uint maxVal = data.Max<uint>();
+            uint[] buckets = new uint[maxVal + 1];
+
+            for(int i = 0; i < data.Count; i++)
+            {
+                buckets[data[i]] += 1;
+            }
+
+            List<uint> list = new List<uint>();
+            for(int i = 0; i < buckets.Length; i++)
+            {
+                for(int j = 0; j < buckets[i]; j++)
+                {
+                    list.Add((uint)i);
+                }
+            }
+
+
+
+            return list;
+
+        } 
+        static void Main(string[] args)
+        {
+            List<uint> data = new List<uint>();
+            Random random = new Random();
+            for(int i = 0; i < 100; i++)
+            {
+                data.Add((uint)random.Next(0, 100));
+            }
+
+            data = CountingSort(data);
+
+            for(int i = 0; i < data.Count; i++)
+            {
+                Console.Write(data[i] + " ");
+            }
+        }
+    }
+}
