d4ef2e1c24f909860133bd8b4513cb64b1aeadb6
VeerShahCodes <shahveer1213@gmail.com>
2025-04-13 13:52:45 -0700

    finished pigeonsort

--- CountingSort/Program.cs
+++ CountingSort/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace CountingSort
+﻿using System.Linq;
+using System.Numerics;
+
+namespace CountingSort
 {
     internal class Program
     {
 
         public static List<int> CountingSort(List<int> data) 
         {
+            
             int maxVal = data.Max<int>();
             int minVal = data.Min<int>();
             int[] buckets = new int[maxVal + 1 - minVal];
@@ -28,21 +32,80 @@
             return list;
 
         } 
-        static void Main(string[] args)
+
+        public static List<KeyValuePair<int, T>>[] PigeonSort<T>(List<KeyValuePair<int,T>> data) where T : IComparable<T>
         {
-            List<int> data = new List<int>();
-            Random random = new Random();
-            for(int i = 0; i < 100; i++)
-            {
-                data.Add(random.Next(-100, 100));
-            }
-
-            data = CountingSort(data);
-
+            int max = int.MinValue;
+            int min = int.MaxValue;
             for(int i = 0; i < data.Count; i++)
             {
-                Console.Write(data[i] + " ");
+                if (data[i].Key > max)
+                {
+                    max = data[i].Key;
+                }
+                if (data[i].Key < min)
+                {
+                    min = data[i].Key;
+                }
             }
+
+            //int[] buckets = new int[max + 1 - min];
+            List<KeyValuePair<int, T>>[] buckets2 = new List<KeyValuePair<int, T>>[max + 1 - min];
+            for (int i = 0; i < buckets2.Length; i++)
+            {
+                buckets2[i] = new List<KeyValuePair<int, T>>();
+            }
+            for (int i = 0; i < data.Count; i++)
+            {
+                buckets2[data[i].Key - min].Add(data[i]);
+               
+            }
+
+            
+
+
+
+            return buckets2;
+        }
+        static void Main(string[] args)
+        {
+            //List<int> data = new List<int>();
+            //Random random = new Random();
+            //for(int i = 0; i < 100; i++)
+            //{
+            //    data.Add(random.Next(-100, 100));
+            //}
+
+            //data = CountingSort(data);
+
+            //for(int i = 0; i < data.Count; i++)
+            //{
+            //    Console.Write(data[i] + " ");
+            //}
+
+
+            List<KeyValuePair<int, string>> list = new List<KeyValuePair<int, string>>();
+
+            list.Add(new KeyValuePair<int, string>(0, "apple"));
+            list.Add(new KeyValuePair<int, string>(1, "banana"));
+            list.Add(new KeyValuePair<int, string>(2, "cat"));
+            list.Add(new KeyValuePair<int, string>(3, "dog"));
+            list.Add(new KeyValuePair<int, string>(4, "elephant"));
+            list.Add(new KeyValuePair<int, string>(5, "fog"));
+            list.Add(new KeyValuePair<int, string>(6, "gorilla"));
+            Random random = new Random();
+            list = list.OrderBy(x => random.Next()).ToList();
+
+            List<KeyValuePair<int, string>>[] sortedBuckets = PigeonSort(list);
+
+            for(int i = 0; i < sortedBuckets.Length; i++)
+            {
+               for(int j = 0; j < sortedBuckets[i].Count; j++)
+                {
+                    Console.WriteLine(sortedBuckets[i][j]);
+                }
+            }
+
         }
     }
 }
